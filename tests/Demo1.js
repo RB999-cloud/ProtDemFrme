f5ca7d66f37bb4b9dbe0bcd20f05297c1fab362c
RB999-cloud <rboa89@gmail.com>
2021-02-10 11:08:42 +0530

    test23

--- tests/Demo1.js
+++ tests/Demo1.js
@@ -42,7 +42,7 @@ it ('Purchasing shirts', function(){
 
  element (by.xpath('//span[.="Add to cart"]')).click();
 
- browser.sleep(4000);
+ browser.sleep(5000);
 
 
 element (by.xpath('//span[contains(.,"Proceed to checkout")]')).click();
